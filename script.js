414140dca94b48844422d4f0794fcbfd94b8ee63
menon-14 <mariana.menon@escola.pr.gov.br>
2025-08-07 08:28:35 -0300

    .,mknjbh6

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
 botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
 
 function mostraPalavrasChave() {
-    alert('fui clicado!');
+    alert('Deseja extrair as palavras?');
 }
